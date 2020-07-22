eb10df968fcf353864659fbdb71bd352edc2c503
3nderboy <66718205+3nderboy@users.noreply.github.com>
2020-07-22 17:09:22 +0200

    Mit Danilo Überarbeitung

---
+++ date/alleUebungen.js
@@ -0,0 +1,15 @@
+export const alleUebungen = [
+  
+  
+  
+  { ÜbungsName: 'Bankdrücken',            Für: 'Brust',               Schwierigkeit: '3 / 5', Umrandung: 'blue',    BildLinks: '4',   key: '1' },
+  { ÜbungsName: 'Bizeps-Curls',           Für: 'Bizeps',              Schwierigkeit: '1 / 5', Umrandung: 'yellow',  BildLinks: '1',   key: '2' },
+  { ÜbungsName: 'Squads',                 Für: 'Beine',               Schwierigkeit: '1 / 5', Umrandung: 'green',   BildLinks: '3',   key: '3' },
+  { ÜbungsName: 'Rudern (im stehen)',     Für: 'Rücken',              Schwierigkeit: '3 / 5', Umrandung: 'brown',   BildLinks: '5',   key: '4' },
+  { ÜbungsName: 'Sit-Ups',                Für: 'Bauchmuskulatur',     Schwierigkeit: '2 / 5', Umrandung: 'green',   BildLinks: '2',   key: '5' },
+  { ÜbungsName: 'Liegestützten',          Für: 'Trizeps',             Schwierigkeit: '2 / 5', Umrandung: 'yellow',  BildLinks: '1',   key: '6' },
+  { ÜbungsName: 'Planke',                 Für: 'Bauchmuskulatur',     Schwierigkeit: '2 / 5', Umrandung: 'green',   BildLinks: '2',   key: '7' },
+
+
+
+];
