8105bb0c14e31bdd893eba19c75771397325a2de
3nderboy <66718205+3nderboy@users.noreply.github.com>
2020-08-03 13:11:04 +0200

    Übungen-Screen fertig (ohne Data)

---
+++ date/alleBilder.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+// Haupt-Muskelmenschen (Zuerst für Übungen, danach für Workouts):
+export const muskelmodellV = require('../assets/Hauptbilder/MuskelmodellV.png');
+export const muskelmodellH = require('../assets/Hauptbilder/MuskelmodellH.png');
+export const muskelmodellWV = require('../assets/Hauptbilder/MuskelmodellWV.png');
+export const muskelmodellWH = require('../assets/Hauptbilder/MuskelmodellWH.png');
+
+// Muskelmenschen mit Muskel onPress:
+export const muskelmodellonclickV = require('../assets/Hauptbilder/MuskelmodellonCV.png');
+export const muskelmodellonclickH = require('../assets/Hauptbilder/MuskelmodellonCH.png');
+export const muskelmodellbrust = require('../assets/Hauptbilder/MuskelmodellBrust.png');
+export const muskelmodellschulter = require('../assets/Hauptbilder/MuskelmodellSchulter.png');
+export const muskelmodellschulterH = require('../assets/Hauptbilder/MuskelmodellSchulterH.png');
+export const muskelmodellnacken = require('../assets/Hauptbilder/MuskelmodellNacken.png');
+
+
+//Alle Muskelmensch-Bilder für die Übungen:
+export const MuskelmenschBilder = {
+    muskelmodellV: require('../assets/Hauptbilder/MuskelmodellV.png'),
+    muskelmodellH: require('../assets/Hauptbilder/MuskelmodellH.png'),
+    muskelmodellonclickV: require('../assets/Hauptbilder/MuskelmodellonCV.png'),
+    muskelmodellonclickH: require('../assets/Hauptbilder/MuskelmodellonCH.png'),
+    muskelmodellbrust: require('../assets/Hauptbilder/MuskelmodellBrust.png'),
+    muskelmodellschulter: require('../assets/Hauptbilder/MuskelmodellSchulter.png'),
+    muskelmodellnacken: require('../assets/Hauptbilder/MuskelmodellNacken.png'),
+    muskelmodellschulterH: require('../assets/Hauptbilder/MuskelmodellSchulterH.png'),
+}
+
+export const MuskelmenschBilderWorkout = {
+    muskelmodellWV: require('../assets/Hauptbilder/MuskelmodellWV.png'),
+    muskelmodellWH: require('../assets/Hauptbilder/MuskelmodellWH.png'),
+    muskelmodellonclickV: require('../assets/Hauptbilder/MuskelmodellonCV.png'),
+    muskelmodellonclickH: require('../assets/Hauptbilder/MuskelmodellonCH.png'),
+    muskelmodellbrust: require('../assets/Hauptbilder/MuskelmodellBrust.png'),
+    muskelmodellschulter: require('../assets/Hauptbilder/MuskelmodellSchulter.png'),
+    muskelmodellnacken: require('../assets/Hauptbilder/MuskelmodellNacken.png'),    //neues Bild für Nacken (Workout andere Farbe)
+    muskelmodellschulterH: require('../assets/Hauptbilder/MuskelmodellSchulterH.png'),
+}
+
+
+
+
+
+//Die 5 Hauptbilder für die Übungen:
+export const hauptbildarm = require('../assets/HauptBilderÜbungen/ÜLinksArme.png');
+export const hauptbildbauch = require('../assets/HauptBilderÜbungen/ÜLinksBauch.png');
+export const hauptbildbeine = require('../assets/HauptBilderÜbungen/ÜLinksBeine.png');
+export const hauptbildbrust = require('../assets/HauptBilderÜbungen/ÜLinksBrust1.png');
+export const hauptbildrücken = require('../assets/HauptBilderÜbungen/ÜLinksRücken.png');
+
+export const HauptBilder = {
+    hauptbildarm: require('../assets/HauptBilderÜbungen/ÜLinksArme.png'),
+    hauptbildbauch: require('../assets/HauptBilderÜbungen/ÜLinksBauch.png'),
+    hauptbildbeine: require('../assets/HauptBilderÜbungen/ÜLinksBeine.png'),
+    hauptbildbrust: require('../assets/HauptBilderÜbungen/ÜLinksBrust1.png'),
+    hauptbildrücken: require('../assets/HauptBilderÜbungen/ÜLinksRücken.png'),
+}
---
+++ date/alleTipps.js
@@ -0,0 +1,18 @@
+const tipp1 = {
+  key: 'tipp1',
+  tippnummer: 'Tipp #1',
+  titel: 'Pausen',
+  erklärung: 'noch keine',
+}
+
+const tipp2 = {
+  key: 'tipp2',
+  tippnummer: 'Tipp #2',
+  titel: 'Ernaehrung',
+  erklärung: 'noch keine',
+}
+
+export const alletipps = [
+  tipp1,
+  tipp2,
+]
