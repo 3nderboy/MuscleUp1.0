c9e86db4c363cb4b634ce6c2c5a48776d269e660
3nderboy <66718205+3nderboy@users.noreply.github.com>
2020-08-07 13:03:30 +0200

    Wabsolvieren läuft + Timer erstellt

---
+++ screens/Nebenscreens/Wabsolvieren.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Button, ScrollView } from 'react-native';
+import { globalStyles } from '../../styles/globalstyle';
+import { CardWabsolvieren } from '../../styles/cards';
+import { Ionicons } from '@expo/vector-icons';
+
+import { alleWorkouts, brustworkouts } from '../../data/alleWorkouts';
+
+export default function Wabsolvieren({ route, navigation }) {
+  const { WorkoutName, Sätze, uebungen } = route.params;
+
+  return (
+    <ScrollView>
+      <View style={globalStyles.containerWorkout}>
+        <Text style={globalStyles.GrosserTitle}>{WorkoutName}</Text>
+
+        <Timer />
+
+        {uebungen.map(
+          (uebung, index) => (
+            <UebungsCard key={index} Index={index + 1} ÜbungsName={uebung.ÜbungsName} Sätze={Sätze} />
+          )
+        )}
+        <View style={{ marginTop: 40, marginBottom: 5 }}>
+          <Button
+            title='Workout fertig'
+            onPress={() => {
+              navigation.navigate('WorkoutScreen')
+            }}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+
+//CardWabsolvieren mit den passenden Infos:
+function UebungsCard(props) {
+  const {Index, ÜbungsName, Sätze} = props;
+
+  return (
+    <CardWabsolvieren>
+      <View style={{ flexDirection: 'row' }}>
+        <Text style={globalStyles.basicTitle}>{Index}. Übung</Text>
+        <Text style={{
+          fontFamily: 'nunitoBold',
+          fontSize: 18,
+          color: 'dimgrey',
+          paddingLeft: 170,
+          paddingRight: 20,
+        }}>Sätze: {Sätze}</Text>
+      </View>
+      <Text style={globalStyles.basicText}>{ÜbungsName}</Text>
+    </CardWabsolvieren>
+  )
+}
+
+
+//Timer:
+function Timer() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(
+    () => {
+      const timeout = setTimeout(() => setSeconds(seconds + 1), 1000);
+      return () => clearTimeout(timeout);
+    },
+    [seconds]
+  )
+
+  return (
+    <View style={{ flexDirection: 'row', paddingVertical: 20 }}>
+      <Ionicons name="md-alarm" size={30} color='dimgrey' style={{ paddingRight: 10 }} />
+      <Text style={globalStyles.basicTitle}>{convertSeconds(seconds)}</Text>
+    </View>
+  )
+}
+
+function convertSeconds(seconds) {
+  const min = Math.floor(seconds/60);
+  const sec = seconds % 60;
+  return `${min}:${sec < 10 ? '0' + sec : sec}`;
+}
+
+
+
+//route.params wird bei WorkoutScreen.js beim Button weitergeleitet.
+//So können auch die Daten hier in Wabsolvieren verwendet werden.
