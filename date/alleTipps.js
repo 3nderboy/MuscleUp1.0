b2ad7bfe32f0b18073e8b3c35380471abcc1139a
3nderboy <66718205+3nderboy@users.noreply.github.com>
2020-08-31 18:05:50 +0200

    First Publish der App

---
+++ date/alleTipps.js
@@ -0,0 +1,86 @@
+//Reihe 1:
+const tipp1R1 = {
+  key: 'tipp1',
+  tippnummer: 'Tipp #1',
+  titel: 'Pausen',
+  erklärung: 'Obwohl es sich auf den ersten Blick nicht besonders positiv für den Muskelaufbau anhört, sind Pausen zwischen den einzelnen Trainingstagen sehr wichtig. Die Muskeln erholen sich und bauen sich erst bei einer längeren Pause richtig auf.',
+}
+
+const tipp2R1 = {
+  key: 'tipp2',
+  tippnummer: 'Tipp #2',
+  titel: 'Ernährung',
+  erklärung: 'noch keine',
+}
+
+const tipp3R1 = {
+  key: 'tipp3',
+  tippnummer: 'Tipp #3',
+  titel: 'Abnehmen',
+  erklärung: 'noch keine',
+}
+
+const tipp4R1 = {
+  key: 'tipp4',
+  tippnummer: 'Tipp #4',
+  titel: 'Muskelaufbau',
+  erklärung: 'noch keine',
+}
+
+
+//Reihe 2:
+const tipp1R2 = {
+  key: 'tipp5',
+  tippnummer: 'Tipp #1',
+  titel: 'Ernährung',
+  erklärung: 'noch keine',
+}
+
+const tipp2R2 = {
+  key: 'tipp6',
+  tippnummer: 'Tipp #2',
+  titel: 'Genügend essen',
+  erklärung: 'noch keine',
+}
+
+const tipp3R2 = {
+  key: 'tipp7',
+  tippnummer: 'Tipp #3',
+  titel: 'Kalorien',
+  erklärung: 'noch keine',
+}
+
+const tipp4R2 = {
+  key: 'tipp8',
+  tippnummer: 'Tipp #4',
+  titel: 'Weniger Süsses',
+  erklärung: 'noch keine',
+}
+
+//Reihe 3:
+const tipp1R3 = {
+  key: 'tipp9',
+  tippnummer: 'Tipp #1',
+  titel: 'Muskelaufbau',
+  erklärung: 'noch keine',
+}
+
+
+//Die Exports der Tipps:
+export const alletippsreihe1 = [
+  tipp1R1,
+  tipp2R1,
+  tipp3R1,
+  tipp4R1,
+]
+
+export const alletippsreihe2 = [
+  tipp1R2,
+  tipp2R2,
+  tipp3R2,
+  tipp4R2,
+]
+
+export const alletippsreihe3 = [
+  tipp1R3,
+]
