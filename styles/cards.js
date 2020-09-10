667b93221c98b8b465d19f899cde93d3e14b4231
3nderboy <66718205+3nderboy@users.noreply.github.com>
2020-09-10 23:12:06 +0200

    Abstände am bearbeiten

---
+++ styles/cards.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
+
+export function CardWabsolvieren(props) {
+    return (
+        <View style={styles.wabsolvierencard}>
+            <View style={styles.wabsolvierencardContent}>
+                { props.children }
+            </View>
+        </View>
+    );
+}
+
+export function TippsCard(props) {
+    return (
+            <View style={styles.tippcard}>
+                <View style={styles.tippContent}>
+                    { props.children }
+                </View>
+            </View>
+    )
+}
+
+export function WorkoutInformationCard(props) {
+    return (
+            <View style={styles.workoutinfoboxcard}>
+                <View style={styles.workoutinfoboxcontent}>
+                    { props.children }
+                </View>
+            </View>
+    )
+}
+
+export function UserInformationCard(props) {
+    return (
+            <View style={styles.userinfoboxcard}>
+                <View style={styles.userinfoboxcontent}>
+                    { props.children }
+                </View>
+            </View>
+    )
+}
+
+export function AufZuCard(props) {
+    return (
+        <View style={styles.aufZuCard}>
+            <View style={styles.aufZuCardContent}>
+                { props.children }
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    wabsolvierencard: {
+        borderRadius: 3,
+        elevation: 2,
+        backgroundColor: 'white',
+        shadowOffset: { width: 1, height: 1 },
+        shadowOpacity: 0.3,
+        shadowRadius: 3,
+        marginBottom: 15,
+    },
+    wabsolvierencardContent: {
+        marginHorizontal: 20,
+        marginVertical: 15,
+    },
+    tippcard: {
+        borderRadius: 23.3,
+        elevation: 4,
+        backgroundColor: 'snow',           // #0000 = invisible
+        borderWidth: 1,
+        borderColor: 'grey',
+        // shadowOffset: { width: 1, height: 1 },
+        // shadowColor: '#333',
+        // shadowOpacity: 0.3,
+        // shadowRadius: 20,
+        paddingLeft: 0,
+    },
+    tippContent: {
+        padding: 0,
+        marginHorizontal: 0,
+        marginLeft: -0.2,
+        marginVertical: 0,
+    },
+    workoutinfoboxcard: {
+        borderRadius: 2,
+        elevation: 2.5,
+        backgroundColor: '#0000',           // #0000 = invisible
+        borderWidth: 1,
+        borderColor: 'grey',
+    },
+    workoutinfoboxcontent: {
+        paddingHorizontal: 10,
+        marginHorizontal: 10,
+        marginLeft: 0,
+        marginVertical: 10,
+    },
+    userinfoboxcard: {
+        borderRadius: 20,
+        elevation: 8,
+        backgroundColor: 'white',           // #0000 = invisible
+        borderWidth: 0.5,
+        borderColor: 'snow',
+        marginHorizontal: 1,
+        //marginBottom: -200
+    },
+    userinfoboxcontent: {
+        paddingHorizontal: 10,
+        marginHorizontal: 10,
+        marginHorizontal: 10,
+        marginVertical: 10,
+    },
+    aufZuCard: {
+        borderRadius: 31.8,
+        elevation: 3,
+        backgroundColor: 'white',           // #0000 = invisible
+        borderWidth: 1.2,
+        borderColor: 'azure',
+        shadowOffset: { width: 1, height: 1 },
+        shadowColor: '#333',
+        shadowOpacity: 0.3,
+        shadowRadius: 2,
+    },
+    aufZuCardContent: {
+        marginHorizontal: 14,
+        marginVertical: 0,
+        marginLeft: 0,
+        marginBottom: 0,
+    },
+});
