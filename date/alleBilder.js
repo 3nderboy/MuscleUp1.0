ff03fa8243a2f5d63f7256accf9846a45fd0f9c8
3nderboy <66718205+3nderboy@users.noreply.github.com>
2020-09-29 23:28:14 +0200

    Übungen am einfügen (Teil 3)

---
+++ date/alleBilder.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+// Haupt-Muskelmenschen (Zuerst für Übungen, danach für Workouts):
+export const muskelmodellV = require('../assets/Hauptbilder/MuskelmodellV.png');
+export const muskelmodellH = require('../assets/Hauptbilder/MuskelmodellH.png');
+export const muskelmodellWV = require('../assets/Hauptbilder/MuskelmodellWV.png');
+export const muskelmodellWH = require('../assets/Hauptbilder/MuskelmodellWH.png');
+
+// Muskelmenschen mit Muskel onPress:
+export const muskelmodellonclickV = require('../assets/Hauptbilder/MuskelmodellonCV.png');
+export const muskelmodellonclickH = require('../assets/Hauptbilder/MuskelmodellonCH.png');
+// export const muskelmodellbrust = require('../assets/Hauptbilder/MuskelmodellBrust.png');
+// export const muskelmodellschulter = require('../assets/Hauptbilder/MuskelmodellSchulter.png');
+// export const muskelmodellschulterH = require('../assets/Hauptbilder/MuskelmodellSchulterH.png');
+// export const muskelmodellnacken = require('../assets/Hauptbilder/MuskelmodellNacken.png');
+
+
+//Alle Muskelmensch-Bilder für die Übungen:
+export const MuskelmenschBilder = {
+    muskelmodellV: require('../assets/Hauptbilder/MuskelmodellV.png'),
+    muskelmodellH: require('../assets/Hauptbilder/MuskelmodellH.png'),
+    muskelmodellonclickV: require('../assets/Hauptbilder/MuskelmodellonCV.png'),
+    muskelmodellonclickH: require('../assets/Hauptbilder/MuskelmodellonCH.png'),
+    muskelmodellbrust: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellBrust.png'),
+    muskelmodellschulter: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellSchulter.png'),
+    muskelmodellnacken: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellNacken.png'),
+    muskelmodellschulterH: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellSchulterH.png'),
+    muskelmodelladduktoren: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellAduktoren.png'),
+    muskelmodellbauchmuskeln: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellBauchmuskeln.png'),
+    muskelmodellbizeps: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellBizeps.png'),
+    muskelmodellgesäss: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellGesäss.png'),
+    muskelmodelllats: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellLats.png'),
+    muskelmodellnackenH: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellNackenH.png'),
+    muskelmodelloberschenkel: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellOberschenkel.png'),
+    muskelmodelloberschenkelH: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellOberschenkelH.png'),
+    muskelmodellrückenstrecker: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellRückenstrecker.png'),
+    muskelmodellseitlichebauchm: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellSeitlichBauchm.png'),
+    muskelmodelltrizeps: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellTrizeps.png'),
+    muskelmodellunterarm: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellUnterarm.png'),
+    muskelmodellwaden: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellWaden.png'),
+    muskelmodellwadenH: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellWadenH.png'),
+    muskelmodellseitlichebauchmH: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellSeitlichBauchmH.png'),
+    muskelmodelllatsV: require('../assets/Hauptbilder/MMBilderÜbungen/MuskelmodellLatsV.png')
+}
+
+export const MuskelmenschBilderWorkout = {
+    muskelmodellWV: require('../assets/Hauptbilder/MuskelmodellWV.png'),
+    muskelmodellWH: require('../assets/Hauptbilder/MuskelmodellWH.png'),
+    muskelmodellonclickV: require('../assets/Hauptbilder/MuskelmodellonCV.png'),
+    muskelmodellonclickH: require('../assets/Hauptbilder/MuskelmodellonCH.png'),
+    muskelmodellWarme: require('../assets/Hauptbilder/MMBilderWorkouts/MuskelmodellWArme.png'),
+    muskelmodellWbauch: require('../assets/Hauptbilder/MMBilderWorkouts/MuskelmodellWBauch.png'),
+    muskelmodellWbeine: require('../assets/Hauptbilder/MMBilderWorkouts/MuskelmodellWBeine.png'),
+    muskelmodellWbeineH: require('../assets/Hauptbilder/MMBilderWorkouts/MuskelmodellWBeineH.png'),
+    muskelmodellWbrust: require('../assets/Hauptbilder/MMBilderWorkouts/MuskelmodellWBrust.png'),
+    muskelmodellWgesäss: require('../assets/Hauptbilder/MMBilderWorkouts/MuskelmodellWGesäss.png'),
+    muskelmodellWrücken: require('../assets/Hauptbilder/MMBilderWorkouts/MuskelmodellWRücken.png'),
+    muskelmodellWschulter: require('../assets/Hauptbilder/MMBilderWorkouts/MuskelmodellWSchulter.png'),
+    muskelmodellWschulterH: require('../assets/Hauptbilder/MMBilderWorkouts/MuskelmodellWSchulterH.png'),
+    muskelmodellWtrizeps: require('../assets/Hauptbilder/MMBilderWorkouts/MuskelmodellWTrizeps.png'),
+    muskelmodellWbauchV: require('../assets/Hauptbilder/MMBilderWorkouts/MuskelmodellWBauchV.png'),
+    muskelmodellWrückenV: require('../assets/Hauptbilder/MMBilderWorkouts/MuskelmodellWRückenV.png')
+}
+
+
+
+
+
+//Die 5 Hauptbilder für die Übungen:
+export const hauptbildarm = require('../assets/HauptBilderÜbungen/ÜLinksArme.png');
+export const hauptbildbauch = require('../assets/HauptBilderÜbungen/ÜLinksBauch.png');
+export const hauptbildbeine = require('../assets/HauptBilderÜbungen/ÜLinksBeine.png');
+export const hauptbildbrust = require('../assets/HauptBilderÜbungen/ÜLinksBrust1.png');
+export const hauptbildrücken = require('../assets/HauptBilderÜbungen/ÜLinksRücken.png');
+
+export const HauptBilder = {
+    hauptbildarm: require('../assets/HauptBilderÜbungen/ÜLinksArme.png'),
+    hauptbildbauch: require('../assets/HauptBilderÜbungen/ÜLinksBauch.png'),
+    hauptbildbeine: require('../assets/HauptBilderÜbungen/ÜLinksBeine.png'),
+    hauptbildbrust: require('../assets/HauptBilderÜbungen/ÜLinksBrust1.png'),
+    hauptbildrücken: require('../assets/HauptBilderÜbungen/ÜLinksRücken.png'),
+}
+
+
+
+//Die drei Hauptbuttons (Zuhause, Mit Hanteln, Mit Geräten):
+export const mithanteln = require('../assets/3Buttons/MitHantelnIcon2.png');
+export const zuhause = require('../assets/3Buttons/ZuhauseIcon.png');
+export const mitgerät = require('../assets/3Buttons/MitGerätIcon.png');
+
+export const dreiButtons = {
+    zuhause: require('../assets/3Buttons/ZuhauseIcon.png'),
+    mithanteln: require('../assets/3Buttons/MitHantelnIcon2.png'),
+    mitgerät: require('../assets/3Buttons/MitGerätIcon.png'),
+    zuhauseOnC: require('../assets/3Buttons/ZuhauseIconOnC.png'),
+    mithantelnOnC: require('../assets/3Buttons/MitHantelnIconOnC.png'),
+    mitgerätOnC: require('../assets/3Buttons/MitGerätIconOnC.png'),
+    mitgeräten: require('../assets/3Buttons/MitGerätenIcon.png'),
+    mitgerätenOnC: require('../assets/3Buttons/MitGerätenIconOnC.png')
+}
+
+
+//Die Feed-Screen Bilder (AufZu-Bilder):
+export const AufZuBilder = {
+    aufZuOwnWorkouts: require('../assets/Feed-ScreenBilder/AufZuEigeneWorkouts.png'),
+    aufZuWorkouts: require('../assets/Feed-ScreenBilder/AufZuWorkouts.png'),
+    aufZuAbout: require('../assets/Feed-ScreenBilder/AufZuAbout.png')
+}
+
+
+//Die Tipps-Bilder:
+export const TippsBilder = {
+    startTipps: require('../assets/TippsBilder/StartTippsBild.png'),
+    trainingTipps: require('../assets/TippsBilder/TrainingTippsBild.png'),
+    ernährungTipps: require('../assets/TippsBilder/ErnährungTippsBild.png'),
+}
---
+++ date/alleWorkouts.js
@@ -0,0 +1,176 @@
+import { bankdrücken, 
+          bizepsCurls, 
+          squats, 
+          rudernimstehen,
+          situps,
+          liegestützen, 
+          planke,
+          schulterdrücken,
+          diamondliegestützen,
+} from '../data/alleUebungen';
+
+
+const workout1 = {
+  WorkoutName: 'Bauch-Workout',
+  Für: 'Bauchmuskulatur',
+  Sonst: 'Unterer Rücken',
+  Beschreibung: 'noch keine',
+  Wie: 'Zuhause',
+  Dauer: '30',            // in min
+  Schwierigkeit: '2',     // x / 5
+  Pausendauer: '30',     // in sek
+  Wiederholungen: '30',
+  WiederholungenmitHanteln: '-',
+  Sätze: '3',
+  uebungen: [
+    situps,
+    planke,
+  ],
+  BildLinks: '2',
+  key: '1'
+}
+
+const workout2 = {
+  WorkoutName: 'Arme-Workout',
+  Für: 'Bizeps und Trizeps',
+  Sonst: 'Unterarmmuskeln',
+  Beschreibung: 'noch keine',
+  Wie: 'Mit Hanteln',
+  Dauer: '50',
+  Schwierigkeit: '2',
+  Pausendauer: '180',
+  Wiederholungen: '12',
+  WiederholungenmitHanteln: '12',
+  Sätze: '4',
+  uebungen: [
+    bizepsCurls,
+  ],
+  BildLinks: '2',
+  key: '2',
+}
+
+const workout3 = {
+  WorkoutName: 'Beine-Workout',
+  Für: 'Oberschenkel',
+  Sonst: 'Aduktoren, Waden',
+  Beschreibung: 'noch keine',
+  Wie: 'Mit Hanteln',
+  Dauer: '50',
+  Schwierigkeit: '2',
+  Pausendauer: '180',
+  Wiederholungen: '15',
+  WiederholungenmitHanteln: '12',
+  Sätze: '3',
+  uebungen: [
+    squats,
+  ],
+  BildLinks: '2',
+  key: '3',
+}
+
+const workout4 = {
+  WorkoutName: 'Brust-Workout',
+  Für: 'Brust',
+  Sonst: 'Oberer Rücken, Schultern',
+  Beschreibung: 'noch keine',
+  Wie: 'Mit Geräten',
+  Dauer: '50',
+  Schwierigkeit: '3',
+  Pausendauer: '120',
+  Wiederholungen: '10',
+  WiederholungenmitHanteln: '12',
+  Sätze: '4',
+  uebungen: [
+    liegestützen,
+    diamondliegestützen,
+    bankdrücken,
+  ],
+  BildLinks: '2',
+  key: '4',
+}
+
+const workout5 = {
+  WorkoutName: 'Schultern-Workout',
+  Für: 'Schultern',
+  Sonst: 'Oberer Rücken, Nacken',
+  Beschreibung: 'noch keine',
+  Wie: 'Mit Hanteln',
+  Dauer: '55',
+  Schwierigkeit: '4',
+  Pausendauer: '180',
+  Wiederholungen: '10',
+  WiederholungenmitHanteln: '12',
+  Sätze: '3',
+  uebungen: [
+    schulterdrücken,
+    rudernimstehen,
+  ],
+  BildLinks: '2',
+  key: '5',
+}
+
+export const exampleWorkout = {
+  WorkoutName: 'Beispiel-Workout',
+  Für: 'Bizeps',
+  Sonst: 'Trizeps, Unterarm',
+  Beschreibung: 'noch keine',
+  Wie: 'Mit Hanteln',
+  Dauer: '10',
+  Schwierigkeit: '2',   
+  Pausendauer: '120',
+  Wiederholungen: '15',
+  WiederholungenmitHanteln: '12',
+  Sätze: '4',
+  uebungen: [
+    schulterdrücken,
+    rudernimstehen,
+  ],
+  BildLinks: '2',
+  key: '6',
+}
+
+export const alleWorkouts = [
+  workout1,
+  workout2,
+  workout3,
+  workout4,
+  workout5,
+];
+
+export const brustworkouts = [
+  workout3,
+  workout4
+];
+
+export const schulternworkouts = [
+  workout1,
+  workout2,
+  workout5,
+];
+
+export const rückenworkouts = [
+
+];
+
+export const armworkouts = [
+
+];
+
+export const trizepsworkouts = [
+
+];
+
+export const bauchworkouts = [
+
+];
+
+export const gesässworkouts = [
+
+];
+
+export const beineworkouts = [
+  
+];
+
+
+
