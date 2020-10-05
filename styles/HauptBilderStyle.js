1ba149ef809a1c5e4882be93c96ca6ae957552d0
3nderboy <66718205+3nderboy@users.noreply.github.com>
2020-10-05 12:32:06 +0200

    Alle Übungen fertig eingefügt

---
+++ styles/HauptBilderStyle.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { hauptbildarm, 
+        hauptbildbauch, 
+        hauptbildbeine, 
+        hauptbildbrust, 
+        hauptbildrücken,
+        } 
+        from '../data/alleBilder';
+
+export const HauptBilderStyles = {
+    übungenBildLinks: {
+        '1': hauptbildarm,
+        '2': hauptbildbauch,
+        '3': hauptbildbeine,
+        '4': hauptbildbrust,
+        '5': hauptbildrücken,
+    }
+};
+
+
+
+
+
+
+
+        // '1': require('../assets/HauptBilderÜbungen/ÜLinksArme.png'),
+        // '2': require('../assets/HauptBilderÜbungen/ÜLinksBauch.png'),
+        // '3': require('../assets/HauptBilderÜbungen/ÜLinksBeine.png'),
+        // '4': require('../assets/HauptBilderÜbungen/ÜLinksBrust1.png'),
+        // '5': require('../assets/HauptBilderÜbungen/ÜLinksRücken.png'),
