c28e045fdac39fa5721d7de9c488742568073095
3nderboy <66718205+3nderboy@users.noreply.github.com>
2020-10-10 14:12:38 +0200

    MuscleUp Abgabe

---
+++ QuellenundLinks.js
@@ -0,0 +1,40 @@
+// Quellen und Links, welche ich verwendet habe, um an unwissenden Code zu kommen oder einfach nur als Hilfe:
+
+//Hinweis: Da ich viele verschiedene Webseite besucht habe, kann ich hier, wie mit meiner Betreuungsperson besprochen,
+//nur die ''Hauptquellen'' angeben. Als Programmierer ist es normal, Code zu verwenden, welcher jemand anderes schon ein Mal
+//geschrieben hat. Trotzdem versuche ich möglichst alle wichtigen ''Hauptquellen'', welche ich für meine kompletten 7200 (3600+
+//Linien Code ohne den Ordner mit allen Daten) Linien Code verwendet habe, hier anzugeben.
+//Zudem gebe ich weiter unten noch an, wo mir mein guter Kollege Danilo Wanner geholfen hat.
+
+
+//Links und Quellen:
+
+//GitHub für das Hochladen des kompletten Codes:              https://github.com/
+//Allgemeine React Native Webseite:                           https://reactnative.dev/
+//Komplette Dokumentation auf der React Native Webseite:      https://reactnative.dev/docs/getting-started
+//Hauptkomponenten auf der React Native Webseite:             https://reactnative.dev/docs/components-and-apis
+//Expo Dokumentationen:                                       https://docs.expo.io/
+//Guides auf der Expo-Webseite:                               https://docs.expo.io/guides/
+//React Navigation Webseite:                                  https://reactnavigation.org/
+//React Navigation Dokumentation:                             https://reactnavigation.org/docs/getting-started
+//ReactJS-Webseite:                                           https://reactjs.org/
+//ReactJS-Webseite Erklärung über die Hooks:                  https://reactjs.org/docs/hooks-intro.html
+//Formik-Webseite für die Validierungen:                      https://formik.org/docs/guides/validation
+//GitHub-AsyncStorage-Dokumentation:                          https://react-native-community.github.io/async-storage/docs/usage
+//StackOverflow-Webseite für kleinere Probleme und Hilfen:    https://stackoverflow.com/
+//Medium-Webseite für kleinere Probleme und Fragen:           https://medium.com/react-native-training
+//Heartbeat für die Navigation innerhalb der App              https://heartbeat.fritz.ai/ 
+//React Native Code für kleinere Probleme und Hilfen:         https://reactnativecode.com/change-image-source-dynamically/ 
+//YouTube-Tutorials von ''The Net Ninja'':                    https://www.youtube.com/channel/UCW5YeuERMmlnqo4oq8vwUpg
+//YouTube-Tutorials von ''DesignIntoCode'':                   https://www.youtube.com/channel/UCPZlE8KsMkumnjEMOcJDxuQ
+
+
+
+
+//In diesen Bereichen meines Codes wurde mir von Danilo Wanner (einem erfahrenen React Native Programmierer) geholfen:
+
+//Erklärung der Hooks (also überall wo Hooks vorkommen)
+//Den Timer auf dem Workout-absolvieren-Screen (Datei: Wabsolvieren.js)
+//Das Speichern der selbst erstellten Workouts mit Hilfe von AsyncStorage (Datei: OwnWorkout.js und createWorkout.js)
+//Das schnellere Laden der einzelnen Übungen auf dem Übungen-Screen (Datei: uebungen.js)
+//Die Top Tap Bar innerhalb der Bottom Tab Bar zu verschachteln (Datei: bottomtab1.js)
